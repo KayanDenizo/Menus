c9a74223d53b1d93fdf36ad76f85b8c574547125
KayanDenizo <kayan.denizo@gmail.com>
2024-04-21 14:39:44 -0300

    Primeiro Commit, para adicionar os arquivos modificados pela primeira vez

---
+++ Menus CSS and JS/JS/hamburgerMenu.js
@@ -0,0 +1,10 @@
+// Define uma largura da sidebar para 250px e a margin esquera (margin left) para 250px
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
